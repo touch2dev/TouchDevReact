896a87dd2976028f3da0cc7449c50eea6930a8d6
nta45 <nathantayele@gmail.com>
2024-06-05 15:52:58 -0400

    front page 50% done

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,59 @@
+'use client';
 import Button from "@/components/Button";
 import Navigation from "@/components/Navigation";
 import Terminal from "@/components/Terminal";
 import Image from "next/image";
+import { useState } from "react";
+import Window from "@/components/Window";
+
 
 export default function Home() {
-  return (<>
-        <div className="flex h-screen w-screen flex-col items-center">
-            <div className="flex flex-row justify-center items-center">
-              <nav className="self-start">
-                  <Navigation />
-              </nav>
-            </div>
-            <div className="h-4/5 w-4/5 flex border-4 border-dotted m-5">
-                <section className="flex flex-row w-1/2 items-start justify-start overflow-y-scroll">
-                    <div className="flex flex-col m-3 p-2">
-                    </div>
-                </section >
-                <section className="w-1/2 m-5 border-l overflow-y-scroll">
-                    <Terminal/>
-                </section >
-            </div>
+  const [isSettingsWindowOpen, setIsSettingsWindowOpen] = useState(false);
+  const [isProfileWindowOpen, setIsProfileWindowOpen] = useState(false);
+  return (
+    <>
+      <div className="home-screen">
+        <header className="header">
+          <button className="settings-icon" onClick={() => setIsSettingsWindowOpen(true)}>⚙️</button>
+          {isSettingsWindowOpen && (<Window show={isSettingsWindowOpen} onClose={setIsSettingsWindowOpen} title={"Settings"}>
+                <p>Settings coming soon</p>
+              </Window>)}
+          <button className="profile-icon" onClick={() => setIsProfileWindowOpen(true)}>👤</button>
+          {isProfileWindowOpen && (<Window show={isProfileWindowOpen} onClose={setIsProfileWindowOpen} title={"Profile"}>
+                <p>Profile info coming soon</p>
+              </Window>)}
+        </header>
+        <div className="title rounded-lg bg-gray-500 px-5 mt-10">
+          <h1>TouchDev</h1>
         </div>
-      </>
+        <div className="get-started">
+          <h2>Get Started</h2>
+          <div className="flex justify-between gap-4">
+            <button className="btn">
+              <div className="icon">➕</div>
+              <div>New File</div>
+            </button>
+            <button className="btn">
+              <div className="icon">📂</div>
+              <div>Open File</div>
+            </button>
+            <button className="btn" onClick={() => {window.location.href = 'https://docs.touchdev.app'}}>
+              <div className="icon">❓</div>
+              <div>Help / Info</div>
+            </button>
+          </div>
+        </div>
+        <div className="recent-files">
+          <h2>Recent Files...</h2>
+            <p className="text-gray-600">functionality coming soon...</p>
+            <br />
+          <div className="file-icons">
+            {/* <div className="file-icon">📄</div>
+            <div className="file-icon">📄</div>
+            <div className="file-icon">📄</div> */}
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
